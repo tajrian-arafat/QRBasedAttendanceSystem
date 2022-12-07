fc4a8f927725321868294bbcc5606a42d958e825
tajrian-arafat <85781752+tajrian-arafat@users.noreply.github.com>
2022-12-07 21:26:35 +0600

    URL Bug Fixed!

--- resources/views/qrcode.blade.php
+++ resources/views/qrcode.blade.php
@@ -29,7 +29,7 @@
     });
 
     function makeRequest (){
-        $.ajax({url: "/getQR", success: function(result){
+        $.ajax({url: "getQR", success: function(result){
             $("#qr-div").html(result);
         }});
     }
